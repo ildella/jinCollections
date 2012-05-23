60af380f59311804a4d0eecbc7b4eca429e1f480
Daniele Dellafiore <ildella@minirockz.(none)>
2012-05-23 12:32:23 +0200

    aggiunto map a GroupBy

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ jinCollections
 ==============
 
 jIntention - jinCollections toolset
+
+jin-collections is a library that helps working with standard Java Collections utility in a "Code by Intenction" way. 
+It is inspired from Smalltalk Collections Utility.
--- src/main/java/jin/collection/util/GroupBy.java
+++ src/main/java/jin/collection/util/GroupBy.java
@@ -1,10 +1,6 @@
 package jin.collection.util;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import jin.collection.core.ChainedOperation;
 import jin.collection.core.Iter;
@@ -17,41 +13,65 @@ import jin.collection.core.ReadAccessor;
  */
 public class GroupBy {
 
-   private ReadAccessor accessor;
+	private ReadAccessor accessor;
 
-   public GroupBy(final String property) {
-      this(new ReadAccessor() {
-         public Object getValue(Object element) {
-            return PropertyUtil.getProperty(element, property);
-         }
-      });
-   }
+	public GroupBy(final String property) {
+		this(new ReadAccessor() {
+			public Object getValue(Object element) {
+				return PropertyUtil.getProperty(element, property);
+			}
+		});
+	}
 
-   public GroupBy(ReadAccessor accessor) {
-      this.accessor = accessor;
-   }
+	public GroupBy(ReadAccessor accessor) {
+		this.accessor = accessor;
+	}
 
-   /**
-    * Raggruppa in base al valore della properti
-    */
-   public Map runOn(Collection elements) {
+	/**
+	 * Groups a new Map of Lists based on the property
+	 */
+	public Map runOn(Collection elements) {
 
-      return (Map) Iter.chain(elements, new HashMap(), new ChainedOperation() {
-         public Object execute(Object element, Object currValue) {
+		return (Map) Iter.chain(elements, new HashMap(), new ChainedOperation() {
+			public Object execute(Object element, Object currValue) {
 
-            Map map = (Map) currValue;
+				Map map = (Map) currValue;
 
-            Object propertyValue = accessor.getValue(element);
-            List propertyList = (List) map.get(propertyValue);
-            if (propertyList == null) {
-               propertyList = new ArrayList();
-               map.put(propertyValue, propertyList);
-            }
-            propertyList.add(element);
-            return map;
-         }
-      });
-   }
+				Object propertyValue = accessor.getValue(element);
+				List propertyList = (List) map.get(propertyValue);
+				if (propertyList == null) {
+					propertyList = new ArrayList();
+					map.put(propertyValue, propertyList);
+				}
+				propertyList.add(element);
+				return map;
+			}
+		});
+	}
+
+	/**
+	 * Groups a new Map of Objects based on the property.
+	 * 
+	 * @throws #{@link IllegalArgumentException} there is more than one value for
+	 *         the given property
+	 */
+	public Map map(List elements) {
+		return (Map) Iter.chain(elements, new HashMap(), new ChainedOperation() {
+			public Object execute(Object element, Object currValue) {
+
+				Map map = (Map) currValue;
+
+				Object keyValue = accessor.getValue(element);
+				Object value = map.get(keyValue);
+				if (value == null) {
+					map.put(keyValue, element);
+				} else {
+					throw new IllegalArgumentException("The list contains more than one object with key: " + keyValue);
+				}
+				return map;
+			}
+		});
+	}
 
 }
 
